891efbe697bf58414e680d40bf615d401e6e4e03
arikim513 <arikim513@gmail.com>
2020-01-23 16:47:06 +0900

    Updates

---
+++ precache-manifest.6e0ed5ff0928721a1e6c41bd4c7b6338.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d2d35379e0786e1dbf9fb1ceec7571a",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "6cb65b12c5513fdf0588",
+    "url": "/movie_app/static/css/main.863c14f9.chunk.css"
+  },
+  {
+    "revision": "975c1a2c1dfdbd009111",
+    "url": "/movie_app/static/js/2.abca4018.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app/static/js/2.abca4018.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6cb65b12c5513fdf0588",
+    "url": "/movie_app/static/js/main.dfe396f2.chunk.js"
+  },
+  {
+    "revision": "ca4b155c3570fabe1dd4",
+    "url": "/movie_app/static/js/runtime-main.98d66c69.js"
+  }
+]);
